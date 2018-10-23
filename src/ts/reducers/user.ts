0c53ca440b9a01899e52769cd618f3d68a69e3ac
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-23 17:46:48 +0100

    added initial reducer for the 'user' property of the redux store

---
+++ src/ts/reducers/user.ts
@@ -0,0 +1,37 @@
+'use strict';
+import {
+  LOGGING_IN, LOGGED_IN, LOG_IN_FAILED
+} from '../actions/actionTypes';
+import { IUser } from '../interfaces/redux';
+import {
+  ILoggingInAction, ILoggedInAction, ILogInFailedAction
+} from '../interfaces/reduxActions';
+
+const initialState: IUser = {
+  isLoggedIn: document.cookie.split(';')
+    .filter((item) => item.includes('session=')).length === 1,
+  isLoggingIn: false,
+};
+
+export default function user(
+  state: IUser = initialState,
+  action: ILoggingInAction | ILoggedInAction | ILogInFailedAction
+): IUser {
+  switch (action.type) {
+    case LOGGING_IN:
+      return({ ...state, isLoggingIn: true });
+
+    case LOGGED_IN:
+      return({
+        ...state,
+        isLoggedIn: true,
+        isLoggingIn: false,
+      });
+
+    case LOG_IN_FAILED:
+      return({ ...state, isLoggingIn: false });
+
+    default:
+      return(state);
+  }
+}
