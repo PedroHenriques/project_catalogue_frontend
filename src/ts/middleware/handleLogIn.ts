d8855539e61a6f6240d494db3b89cbd59f6e690b
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-23 17:26:11 +0100

    added the redux middleware that will process the login action

---
+++ src/ts/middleware/handleLogIn.ts
@@ -0,0 +1,25 @@
+'use strict';
+import { Dispatch, Store } from 'redux';
+import { LOG_IN } from '../actions/actionTypes';
+import { login } from '../utils/backendFacade';
+import { IState } from '../interfaces/redux';
+import { IFluxStandardAction, ILogInAction } from '../interfaces/reduxActions';
+
+const handleLogIn = (store: Store<IState, IFluxStandardAction>) =>
+  (next: Dispatch<IFluxStandardAction>) => (action: IFluxStandardAction) => {
+    if (action.type !== LOG_IN) { return(next(action)); }
+
+    const state = store.getState();
+    if (!state.user.isLoggedIn && !state.user.isLoggingIn) {
+      login({
+        email: (action as ILogInAction).payload.email,
+        password: (action as ILogInAction).payload.password,
+      })
+      .catch(error => {
+        return;
+      });
+    }
+    return;
+  };
+
+export default handleLogIn;
