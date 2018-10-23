0034512171def5f50acd9925cfcb81e47b89bba7
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-23 16:28:48 +0100

    added react component for the login form

---
+++ src/ts/components/LoginForm.tsx
@@ -0,0 +1,10 @@
+'use strict';
+import * as React from 'react';
+
+interface ILoginFormState {
+  email: string,
+  password: string,
+}
+
+export default class LoginForm extends React.Component<{}, ILoginFormState> {
+}
