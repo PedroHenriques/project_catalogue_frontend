dd1df6bf0b6bc05f227fb4d030e68be680ec9aad
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-23 18:38:12 +0100

    added redux action for logging out

--- src/ts/actions/actionTypes.ts
+++ src/ts/actions/actionTypes.ts
@@ -5,4 +5,6 @@ export const LOGGING_IN = 'LOGGING_IN';
 export const LOGGED_IN = 'LOGGED_IN';
 export const LOG_IN_FAILED = 'LOG_IN_FAILED';
 
+export const LOG_OUT = 'LOG_OUT';
+
 export const SHOW_LOG_IN = 'SHOW_LOG_IN';
