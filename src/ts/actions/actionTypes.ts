dfefb3c9aabf8f3b08734e1b412196cd023542e2
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-23 17:43:26 +0100

    fixed typo in redux action name

--- src/ts/actions/actionTypes.ts
+++ src/ts/actions/actionTypes.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 export const LOG_IN = 'LOG_IN';
-export const LOGGIN_IN = 'LOGGIN_IN';
+export const LOGGING_IN = 'LOGGING_IN';
 export const LOGGED_IN = 'LOGGED_IN';
 export const LOG_IN_FAILED = 'LOG_IN_FAILED';
 
