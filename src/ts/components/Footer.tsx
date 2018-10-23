30cf5ca0363f703d1d6fa6182a1128bfb0cc7b1f
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-23 16:27:34 +0100

    added react component for the footer

---
+++ src/ts/components/Footer.tsx
@@ -0,0 +1,10 @@
+'use strict';
+import * as React from 'react';
+
+export default class Footer extends React.Component<{}, {}> {
+  render() {
+    return(
+      <div id='footer'>this is the footer</div>
+    );
+  }
+}
