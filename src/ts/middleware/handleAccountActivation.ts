f9c4e5cdcc770d4c764bc7bf50d6149d259830a4
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-24 00:37:12 +0100

    added the redux middleware that activates on account activation redux actions

---
+++ src/ts/middleware/handleAccountActivation.ts
@@ -0,0 +1,35 @@
+'use strict';
+import { Dispatch, Store } from 'redux';
+import { ACTIVATE_ACCOUNT } from '../actions/actionTypes';
+import {
+  activatingAccount, activatedAccount, showLogIn
+} from '../actions/creators';
+import { activateAccount } from '../utils/backendFacade';
+import { IState } from '../interfaces/redux';
+import {
+  IFluxStandardAction, IActivateAccountAction
+} from '../interfaces/reduxActions';
+
+const handleAccountActivation = (store: Store<IState, IFluxStandardAction>) =>
+  (next: Dispatch<IFluxStandardAction>) => (action: IFluxStandardAction) => {
+    if (action.type !== ACTIVATE_ACCOUNT) { return(next(action)); }
+
+    const state = store.getState();
+    if (!state.user.isLoggedIn && !state.user.isLoggingIn) {
+      store.dispatch(activatingAccount());
+      activateAccount({
+        email: (action as IActivateAccountAction).meta.query.email,
+        token: (action as IActivateAccountAction).meta.query.token,
+      })
+      .then(() => {
+        store.dispatch(activatedAccount());
+        store.dispatch(showLogIn());
+      })
+      .catch(error => {
+        store.dispatch(showLogIn());
+      });
+    }
+    return;
+  };
+
+export default handleAccountActivation;
