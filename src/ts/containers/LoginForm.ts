bf4f1fffbb1c6ff205c229bc8cdcc8b463103176
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-23 16:52:32 +0100

    added container for the LoginForm react component

---
+++ src/ts/containers/LoginForm.ts
@@ -0,0 +1,32 @@
+'use strict';
+import * as React from 'react';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import LoginFormComponent from '../components/LoginForm';
+import { authenticate } from '../actions/creators';
+import { IState } from '../interfaces/redux';
+
+interface IMapStateToProps {
+}
+
+interface IMapDispatchToProps {
+  handleSubmit: (args: { email: string, pw: string }) => void,
+}
+
+const mapStateToProps = (state: IState): IMapStateToProps => {
+  return({});
+};
+
+const mapDispatchToProps = (
+  dispatch: Dispatch
+): IMapDispatchToProps => {
+  return({
+    handleSubmit: (args: { email: string, pw: string }): void => {
+      dispatch(authenticate({ ...args }));
+    },
+  });
+};
+
+export default connect(
+  mapStateToProps, mapDispatchToProps
+)(LoginFormComponent);
