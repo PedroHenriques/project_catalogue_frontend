3087d8a218c1718811db69f9416d61df13760e44
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-23 17:47:14 +0100

    registered the user reducer

--- src/ts/reducers/index.ts
+++ src/ts/reducers/index.ts
@@ -1,8 +1,9 @@
 'use strict';
 import { combineReducers } from 'redux';
 import { reducer as routerReducer } from '../config/router';
+import user from './user';
 import { IState } from '../interfaces/redux';
 
 export const reducer = combineReducers<IState>({
-  location: routerReducer
+  location: routerReducer, user
 });
