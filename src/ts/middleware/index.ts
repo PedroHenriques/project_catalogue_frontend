bbe5348bc7b40e5d52f9132fcfd83b8e76f7d80e
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-23 17:27:21 +0100

    registered the redux middleware that handles logging in

--- src/ts/middleware/index.ts
+++ src/ts/middleware/index.ts
@@ -1,7 +1,8 @@
 'use strict';
 import * as Redux from 'redux';
 import { middleware as routerMiddleware } from '../config/router';
+import handleLogIn from './handleLogIn';
 
 export const middleware = Redux.applyMiddleware(
-  routerMiddleware
+  routerMiddleware, handleLogIn
 );
