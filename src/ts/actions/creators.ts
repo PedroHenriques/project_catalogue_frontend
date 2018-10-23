fff1facdecc39b5829349a9494a11a1545817781
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-23 16:50:48 +0100

    added redux action creator for the log in action

--- src/ts/actions/creators.ts
+++ src/ts/actions/creators.ts
@@ -1,3 +1,12 @@
 'use strict';
 import * as ActionTypes from '../actions/actionTypes';
 import * as ActionInterfaces from '../interfaces/reduxActions';
+
+export function authenticate(
+  payload: { email: string, pw: string }
+): ActionInterfaces.ILogInAction {
+  return({
+    type: ActionTypes.LOG_IN,
+    payload,
+  });
+}
