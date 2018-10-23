e540318bacfb59449384ac9f8e726b4c337720c4
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-24 00:12:21 +0100

    registered the redux middleware that handles register actions

--- src/ts/middleware/index.ts
+++ src/ts/middleware/index.ts
@@ -3,7 +3,8 @@ import * as Redux from 'redux';
 import { middleware as routerMiddleware } from '../config/router';
 import handleLogIn from './handleLogIn';
 import handleLogOut from './handleLogOut';
+import handleRegistration from './handleRegistration';
 
 export const middleware = Redux.applyMiddleware(
-  routerMiddleware, handleLogIn, handleLogOut
+  routerMiddleware, handleLogIn, handleLogOut, handleRegistration
 );
