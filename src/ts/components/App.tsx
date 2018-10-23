f35d6a80a07ea225ef77e7692eeac8afb49ff504
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-23 16:28:18 +0100

    added the major react components to the DOM

--- src/ts/components/App.tsx
+++ src/ts/components/App.tsx
@@ -1,10 +1,16 @@
 'use strict';
 import * as React from 'react';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 export default class App extends React.Component<{}, {}> {
   render() {
     return(
       <div id='app'>
+        <Header/>
+        <Main/>
+        <Footer/>
       </div>
     );
   }
