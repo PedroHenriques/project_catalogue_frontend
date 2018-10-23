d32fb4352e4a40fdda5cbf4bb4c9a12da63e39ad
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-23 16:35:43 +0100

    added method to the login form to handle changes to the form input fields

--- src/ts/components/LoginForm.tsx
+++ src/ts/components/LoginForm.tsx
@@ -16,10 +16,27 @@ export default class LoginForm extends React.Component<{}, ILoginFormState> {
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   public handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
     event.preventDefault();
+    return;
+  }
+
+  public handleChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    switch (event.currentTarget.id) {
+      case 'login-email':
+        this.setState({ email: event.currentTarget.value });
+        break;
+
+      case 'login-pw':
+        this.setState({ password: event.currentTarget.value });
+        break;
+
+      default:
+        break;
+    }
     return;
   }
 }
