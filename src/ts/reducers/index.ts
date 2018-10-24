c8c0c60c13a606b749dd19e7c41ad3b5770ed190
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-24 11:26:13 +0100

    registered the redux reducer for the resetPw store property

--- src/ts/reducers/index.ts
+++ src/ts/reducers/index.ts
@@ -3,8 +3,9 @@ import { combineReducers } from 'redux';
 import { reducer as routerReducer } from '../config/router';
 import user from './user';
 import ui from './ui';
+import resetPW from './resetPW';
 import { IState } from '../interfaces/redux';
 
 export const reducer = combineReducers<IState>({
-  location: routerReducer, user, ui
+  location: routerReducer, user, ui, resetPW
 });
