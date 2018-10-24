bc830ea2242acb313685f11225f3d9fffef06af6
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-24 01:16:45 +0100

    added redux reducer for the 'ui' property of the redux store

---
+++ src/ts/reducers/ui.ts
@@ -0,0 +1,46 @@
+'use strict';
+import {
+  HOME, SHOW_LOG_IN, SHOW_REGISTER, LOGGED_IN
+} from '../actions/actionTypes';
+import { IUi } from '../interfaces/redux';
+import {
+  IHomeAction, IShowLogInAction, IShowRegisterAction, ILoggedInAction
+} from '../interfaces/reduxActions';
+
+const initialState: IUi = {
+  showHome: true,
+  showLogin: false,
+  showRegister: false,
+};
+
+export default function ui(
+  state: IUi = initialState,
+  action: IHomeAction | IShowLogInAction | IShowRegisterAction | ILoggedInAction
+): IUi {
+  switch (action.type) {
+    case HOME:
+    case LOGGED_IN:
+      return({
+        showHome: true,
+        showLogin: false,
+        showRegister: false,
+      });
+
+    case SHOW_LOG_IN:
+      return({
+        showHome: false,
+        showLogin: true,
+        showRegister: false,
+      });
+
+    case SHOW_REGISTER:
+      return({
+        showHome: false,
+        showLogin: false,
+        showRegister: true,
+      });
+
+    default:
+      return(state);
+  }
+}
