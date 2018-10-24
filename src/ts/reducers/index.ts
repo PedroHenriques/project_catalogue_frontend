049a47751c668977fcd94ef75c683e06569c0da7
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-24 01:17:02 +0100

    registered the ui redux reducer

--- src/ts/reducers/index.ts
+++ src/ts/reducers/index.ts
@@ -2,8 +2,9 @@
 import { combineReducers } from 'redux';
 import { reducer as routerReducer } from '../config/router';
 import user from './user';
+import ui from './ui';
 import { IState } from '../interfaces/redux';
 
 export const reducer = combineReducers<IState>({
-  location: routerReducer, user
+  location: routerReducer, user, ui
 });
