90a219d8f94536e4cea3978e49c491b222d0f25e
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-24 19:32:32 +0100

    refactored the PropertyList component to handle the embeded google maps rendering

--- src/ts/components/PropertyList.tsx
+++ src/ts/components/PropertyList.tsx
@@ -6,11 +6,68 @@ export interface IProps {
   properties: IProperty[] | null,
 }
 
-export default class PropertyList extends React.Component<IProps, {}> {
+export interface IState {
+  renderMap: boolean,
+}
+
+export default class PropertyList extends React.Component<IProps, IState> {
+  private timeoutId: NodeJS.Timeout | null = null;
+
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+      renderMap: typeof google !== 'undefined',
+    };
+
+    this.checkGoogleMap = this.checkGoogleMap.bind(this);
+  }
+
+  private checkGoogleMap(): void {
+    if (typeof google !== 'undefined') {
+      this.setState({ renderMap: true });
+    } else {
+      this.timeoutId = global.setTimeout(() => {
+        this.checkGoogleMap();
+      }, 1000);
+    }
+  }
+
+  componentDidMount() {
+    this.checkGoogleMap();
+  }
+
+  componentWillUnmount() {
+    if (this.timeoutId !== null) {
+      global.clearTimeout(this.timeoutId);
+    }
+  }
+
   render() {
+    let map: google.maps.Map | null = null;
+    if (this.state.renderMap) {
+      try {
+        map = new google.maps.Map(
+          document.getElementById('map'), { zoom: 4 }
+        );
+      } catch (error) {
+        map = null;
+      }
+    }
+
     let children: JSX.Element[] = [];
     if (this.props.properties !== null) {
       children = this.props.properties.map(property => {
+        if (map !== null) {
+          const Marker = new google.maps.Marker({
+            position: {
+              lat: property.geoLocationLat,
+              lng:  property.geoLocationLong
+            },
+            map,
+          });
+        }
+
         return(
         <div id={`property-${property.id}`} key={`property-${property.id}`}
         className='property-card'>
