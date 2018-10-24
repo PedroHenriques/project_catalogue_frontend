c2ff81201372b03682bb7df3ed056762260ee698
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-24 11:57:05 +0100

    added the redux middleware that activates on a reset password redux action

---
+++ src/ts/middleware/handleResetPw.ts
@@ -0,0 +1,29 @@
+'use strict';
+import { Dispatch, Store } from 'redux';
+import { RESET_PW } from '../actions/actionTypes';
+import { showLogIn } from '../actions/creators';
+import { pwReset } from '../utils/backendFacade';
+import { IState } from '../interfaces/redux';
+import {
+  IFluxStandardAction, IResetPwAction
+} from '../interfaces/reduxActions';
+
+const handleResetPw = (store: Store<IState, IFluxStandardAction>) =>
+  (next: Dispatch<IFluxStandardAction>) => (action: IFluxStandardAction) => {
+    if (action.type !== RESET_PW) { return(next(action)); }
+
+    const state = store.getState();
+    if (!state.user.isLoggedIn && !state.user.isLoggingIn) {
+      pwReset({ ...(action as IResetPwAction).payload })
+      .then(() => {
+        store.dispatch(showLogIn());
+      })
+      .catch(error => {
+        window.alert(error);
+        store.dispatch(showLogIn());
+      });
+    }
+    return;
+  };
+
+export default handleResetPw;
