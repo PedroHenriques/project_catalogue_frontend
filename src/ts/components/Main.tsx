9ede225e3a2e51f457a8f35949e1378f62d98357
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-24 02:31:01 +0100

    added another value to the string literal of elements to render ini the Main component

--- src/ts/components/Main.tsx
+++ src/ts/components/Main.tsx
@@ -4,7 +4,7 @@ import LoginForm from '../containers/LoginForm';
 import RegisterForm from '../containers/RegisterForm';
 
 export interface IProps {
-  componentToRender: 'home' | 'login' | 'register',
+  componentToRender: 'home' | 'login' | 'register' | 'lostPw',
 }
 
 export default class Main extends React.Component<IProps, {}> {
