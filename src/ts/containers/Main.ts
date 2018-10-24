85b55fc7bfb02df55dedfe4642ff91c5a895073f
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-24 01:19:24 +0100

    added conatiner for the Main component

---
+++ src/ts/containers/Main.ts
@@ -0,0 +1,38 @@
+'use strict';
+import * as React from 'react';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import MainComponent from '../components/Main';
+import { IState } from '../interfaces/redux';
+
+interface IMapStateToProps {
+  componentToRender: 'home' | 'login' | 'register',
+}
+
+interface IMapDispatchToProps {
+}
+
+const mapStateToProps = (state: IState): IMapStateToProps => {
+  let renderName: 'home' | 'login' | 'register' = 'home';
+  if (state.ui.showLogin) {
+    renderName = 'login';
+  } else if (state.ui.showRegister) {
+    renderName = 'register';
+  } else if (state.ui.showHome) {
+    renderName = 'home';
+  }
+
+  return({
+    componentToRender: renderName,
+  });
+};
+
+const mapDispatchToProps = (
+  dispatch: Dispatch
+): IMapDispatchToProps => {
+  return({});
+};
+
+export default connect(
+  mapStateToProps, mapDispatchToProps
+)(MainComponent);
