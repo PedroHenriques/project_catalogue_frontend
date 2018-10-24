48a3a1f313965fb312f823939946ad50713383e9
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-24 01:19:01 +0100

    updated the login in Main component's render(), to show the correct content based on the application's state

--- src/ts/components/Main.tsx
+++ src/ts/components/Main.tsx
@@ -9,8 +9,28 @@ export interface IProps {
 
 export default class Main extends React.Component<IProps, {}> {
   render() {
+    let child: JSX.Element | null;
+    switch (this.props.componentToRender) {
+      case 'home':
+        child = <p>Property list goes here</p>;
+        break;
+
+      case 'login':
+        child = <LoginForm />;
+        break;
+
+      case 'register':
+        child = <RegisterForm />;
+        break;
+
+      default:
+        child = null;
+        break;
+    }
+
     return(
       <div id='main'>
+        {child}
       </div>
     );
   }
