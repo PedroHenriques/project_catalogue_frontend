8055a5a03caaad6274e97e94dc851d9dc855d8f9
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-24 02:40:38 +0100

    registered the redux middleware that activates on request password reset actions

--- src/ts/middleware/index.ts
+++ src/ts/middleware/index.ts
@@ -5,8 +5,9 @@ import handleLogIn from './handleLogIn';
 import handleLogOut from './handleLogOut';
 import handleRegistration from './handleRegistration';
 import handleAccountActivation from './handleAccountActivation';
+import handleLostPw from './handleLostPw';
 
 export const middleware = Redux.applyMiddleware(
   routerMiddleware, handleLogIn, handleLogOut, handleRegistration,
-  handleAccountActivation
+  handleAccountActivation, handleLostPw
 );
