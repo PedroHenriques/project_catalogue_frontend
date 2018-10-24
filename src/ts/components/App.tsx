7862d7c0d7aef3ba87293bca7b6150a41519fe79
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-24 01:19:52 +0100

    switched the import of the Main component with the associated container

--- src/ts/components/App.tsx
+++ src/ts/components/App.tsx
@@ -1,7 +1,7 @@
 'use strict';
 import * as React from 'react';
 import Header from '../containers/Header';
-import Main from './Main';
+import Main from '../containers/Main';
 import Footer from './Footer';
 
 export default class App extends React.Component<{}, {}> {
