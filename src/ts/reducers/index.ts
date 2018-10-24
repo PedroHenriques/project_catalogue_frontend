d0e109ddc4232d7233d7dfe5149c04a7a17971bb
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-24 16:45:07 +0100

    registered the redux reducer for the 'properties' property of the redux store

--- src/ts/reducers/index.ts
+++ src/ts/reducers/index.ts
@@ -4,8 +4,9 @@ import { reducer as routerReducer } from '../config/router';
 import user from './user';
 import ui from './ui';
 import resetPW from './resetPW';
+import properties from './properties';
 import { IState } from '../interfaces/redux';
 
 export const reducer = combineReducers<IState>({
-  location: routerReducer, user, ui, resetPW
+  location: routerReducer, user, ui, resetPW, properties
 });
