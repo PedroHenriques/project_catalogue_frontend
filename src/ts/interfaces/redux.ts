9a8782890d91e11c735ef8832bf7614721ddc415
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-24 01:16:13 +0100

    added 'ui' property to the redux store interface

--- src/ts/interfaces/redux.ts
+++ src/ts/interfaces/redux.ts
@@ -4,9 +4,16 @@ import { LocationState } from 'redux-first-router';
 export interface IState {
   location: LocationState,
   user: IUser,
+  ui: IUi,
 }
 
 export interface IUser {
   isLoggedIn: boolean,
   isLoggingIn: boolean,
+}
+
+export interface IUi {
+  showHome: boolean,
+  showLogin: boolean,
+  showRegister: boolean,
 }
