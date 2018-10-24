0c7a7bf1f6827081245402ea473ea4ab6fd9641e
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-24 01:18:07 +0100

    added props to the Main component

--- src/ts/components/Main.tsx
+++ src/ts/components/Main.tsx
@@ -3,7 +3,11 @@ import * as React from 'react';
 import LoginForm from '../containers/LoginForm';
 import RegisterForm from '../containers/RegisterForm';
 
-export default class Main extends React.Component<{}, {}> {
+export interface IProps {
+  componentToRender: 'home' | 'login' | 'register',
+}
+
+export default class Main extends React.Component<IProps, {}> {
   render() {
     return(
       <div id='main'>
