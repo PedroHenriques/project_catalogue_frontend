fa81e4f2df24a05384860deebd6a9e59eab944e3
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-24 02:32:33 +0100

    updated the Main container to check if the LostPassword page should be rendered

--- src/ts/containers/Main.ts
+++ src/ts/containers/Main.ts
@@ -6,20 +6,22 @@ import MainComponent from '../components/Main';
 import { IState } from '../interfaces/redux';
 
 interface IMapStateToProps {
-  componentToRender: 'home' | 'login' | 'register',
+  componentToRender: 'home' | 'login' | 'register' | 'lostPw',
 }
 
 interface IMapDispatchToProps {
 }
 
 const mapStateToProps = (state: IState): IMapStateToProps => {
-  let renderName: 'home' | 'login' | 'register';
+  let renderName: 'home' | 'login' | 'register' | 'lostPw';
   if (state.ui.showLogin) {
     renderName = 'login';
   } else if (state.ui.showRegister) {
     renderName = 'register';
   } else if (state.ui.showHome) {
     renderName = 'home';
+  } else if (state.ui.showLostPw) {
+    renderName = 'lostPw';
   } else {
     renderName = 'home';
   }
