e9e9fac41452935c5da6c788c58cd35b8653cc6e
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-27 19:03:58 +0100

    removed call to window.alert()

--- src/ts/middleware/handleResetPw.ts
+++ src/ts/middleware/handleResetPw.ts
@@ -16,7 +16,6 @@ const handleResetPw = (store: Store<IState, IFluxStandardAction>) =>
     if (!state.user.isLoggedIn && !state.user.isLoggingIn) {
       pwReset({ ...(action as IResetPwAction).payload })
       .then(() => {
-        window.alert('Your password was successfully changed.');
         store.dispatch(showLogIn());
       })
       .catch(error => {
