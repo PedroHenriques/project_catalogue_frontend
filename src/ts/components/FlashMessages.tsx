52f287bc324bc8c80cb3fae067a2f8d53175fcf9
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-27 16:52:31 +0100

    added component for flash messages

---
+++ src/ts/components/FlashMessages.tsx
@@ -0,0 +1,23 @@
+'use strict';
+import * as React from 'react';
+
+export interface IProps {
+  messages: string[],
+}
+
+export default class FlashMessages extends React.Component<IProps, {}> {
+  render() {
+    const children: JSX.Element[] = this.props.messages.map((msg, index) => {
+      return(
+        <div key={`flashMsg${index}`} id={`flashMsg${index}`}
+        className='flashMsg'>
+          <p>{ msg }</p>
+        </div>
+      );
+    });
+
+    return(
+      <div id='flash-messages'>{ children }</div>
+    );
+  }
+}
