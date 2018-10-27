6935544cbcf5065f6caf9eadd2cb517fca37b91d
Pedro Henriques <pedro.henriques@logon-consulting.de>
2018-10-27 18:58:43 +0100

    added import

--- src/ts/middleware/handleAccountActivation.ts
+++ src/ts/middleware/handleAccountActivation.ts
@@ -2,7 +2,7 @@
 import { Dispatch, Store } from 'redux';
 import { ACTIVATE_ACCOUNT } from '../actions/actionTypes';
 import {
-  activatingAccount, activatedAccount, showLogIn
+  activatingAccount, activatedAccount, showLogIn, flash
 } from '../actions/creators';
 import { activateAccount } from '../utils/backendFacade';
 import { IState } from '../interfaces/redux';
